cde5acec271f55bc1d34896787bb41ec8cdb6ea8
luis caro <luis.carosalgado1@gmail.com>
2023-10-14 00:11:41 -0300

    database creaada

--- server/schema.sql
+++ server/schema.sql
@@ -1,1 +1,26 @@
 CREATE DATABASE  tareas;
+
+USE tareas;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(225),
+    email VARCHAR(225) UNIQUE NOT NULL,
+    password VARCHAR(225)
+);
+
+CREATE TABLE tareas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(225),
+    completed BOOLEAN DEFAULT false,
+    user_id INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE share_tareas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tareas_id INT,
+    user_id INT,
+    FOREIGN KEY (tareas_id) REFERENCES tareas(id),
+    FOREIGN KEY (user_id) REFERENCES users(id) 
+);
