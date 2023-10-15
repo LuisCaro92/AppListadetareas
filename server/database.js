de47a1ab48cca70063c9ba0abafa6dbed10896e2
luis caro <luis.carosalgado1@gmail.com>
2023-10-15 16:04:58 -0300

    database terminada

--- server/database.js
+++ server/database.js
@@ -22,13 +22,61 @@ export async function getTareasById(id) {
 }
 
 export async function getTareas(id) {
-  const [row] = await pool.query(`SELECT * FROM tareas WHERE id = ? `, [id]);
-  return row[0];
+  const [rows] = await pool.query(`SELECT * FROM tareas WHERE id = ? `, [id]);
+  return rows[0];
 }
 
 export async function getSharedTareasById(id) {
-    const [row] = await pool.query(`SELECT * FROM shared_tareas WHERE tarea_id = ?`, [id]);
-    return row[0];
+    const [rows] = await pool.query(`SELECT * FROM shared_tareas WHERE tarea_id = ?`, [id]);
+    return rows[0];
   }
+
+
+export async function getUserById(id){
+    const [rows] = await pool.query(`SELECT * FROM users WHERE id = ?`, [id]);
+    return rows[0];
+}
+
+export async function getUserByEmail(email){
+    const [rows] = await pool.query(`SELECT * FROM users WHERE email = ?`,[email]);
+    return rows[0];
+}
+
+
+export async function createTareas(user_id, title){
+    const [result] = await pool.query(
+        `INSERT INTO tareas (user_id, title) VALUES ('?, ?)`,[user_id, title]
+    );
+    const tareaId = result.insertId;
+    return getTareas(tareaId);
+}
+
+
+export async function  deleteTareas(id){
+    const [result] = await pool.query(
+        `DELETE FROM tareas WHERE id = ?;`,
+        [id]
+    );
+}
+
+export  async function toogleCompleted(id, value){
+    const newValue = value === true ? "TRUE" : "FALSE";
+    const [result] = await pool.query(
+        `UPDATE tareas
+        SET completed = ${newValue}
+        WHERE id = ?;`, [id]
+    );
+    return result;
+}
+
+
+
+export async function sharedTarea(tarea_id, user_id, shared_with_id){
+    const [result] = await pool.query(
+        `INSERT INTO shared_tareas (tarea_id, user_id, shared_with_id)
+        VALUES (?, ?, ?);`,[tarea_id, user_id, shared_with_id]
+    );
+    return result.insertId;
+}
 
 getTareas(1);
